dde67649944953c6f6c929382cb50acf4932403a
osama <osamaabido99@gmail.com>
2025-05-14 17:23:19 +0300

    kaggle3

--- models/PeronActivityClassifer.py
+++ models/PeronActivityClassifer.py
@@ -1,19 +1,14 @@
-import albumentations as A
 import torch.nn as nn
-from datetime import datetime
-from albumentations.pytorch import ToTensorV2
-from torch.utils.data import DataLoader
-import torchvision.models as models
 
 class ClassiferNN(nn.Module):
     def __init__(self, person_feature_extraction, num_classes):
         super(ClassiferNN, self).__init__()
-        self.feature_extraction = nn.Sequential(*list(person_feature_extraction.resnet50.children())[:-1])
+        self.feature_extraction = nn.Sequential(*list(person_feature_extraction.resnet50.children())[:-2])
 
         for param in self.feature_extraction.parameters():
             param.requires_grad = False
         
-        self.pool = nn.AdaptiveMaxPool2d((1, 2048))  # [12, 2048] -> [1, 2048]
+        self.pool = nn.AdaptiveMaxPool2d((1, 1))  
         
         self.fc = nn.Sequential(
             nn.Linear(2048, 1024),
@@ -27,11 +22,8 @@ class ClassiferNN(nn.Module):
         batch, bounding_box, c, h, w = x.shape 
         x = x.view(batch*bounding_box, c, h, w) 
         x = self.feature_extraction(x) 
-
-        x = x.view(batch, bounding_box, -1) 
         x = self.pool(x) 
-        
-        x = x.squeeze(dim=1) 
+        x = x.view(batch, bounding_box, -1) 
+        x = x.mean(dim=1) 
         x = self.fc(x) 
         return x
-    
