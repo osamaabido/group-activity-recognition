34ddd7b20e2e63480a3fd66f12ecbf57c2534a28
osamaabido <osamaabidoo99@gmail.com>
2025-09-09 17:07:26 +0300

    osos

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,6 @@ from .GroupActivityClassifer import Group_Activity_Classifer
 from .PeronActivityClassifer import ClassiferNN
 from .LstmGroup import LSTMGroup
 from .Lstmperson import LSTMPerson
-from .Baseline7 import Person_Activity_Temporal , Group_Activity_Temporal
+from .Baseline7a import Person_Activity_Temporal
+from .Baseline7b import Group_Activity_Temporal
+
