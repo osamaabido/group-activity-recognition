63894786b89c28ba9ef8131d468fdb20539bfa37
osamaabido <osamaabidoo99@gmail.com>
2026-07-23 15:42:21 +0300

    osa

--- configs/Paths.py
+++ configs/Paths.py
@@ -3,29 +3,30 @@
 class File_Paths():
 
     Project_Root = r"H:\Group-Activity-Recognition"
-    Data_Dir = r"GroupActivityDataset" 
-    Annot_Path = r"H:\Group-Activity-Recognition\dataset\annot_all.pkl"
-    Videos_Path: r"H:\Group-Activity-Recognition\dataset\videos"
+    Data_Dir = r"/kaggle/input/datasets/sherif31/group-activity-recognition-volleyball" 
+    Annot_Path = r"/kaggle/input/datasets/sherif31/group-activity-recognition-volleyball/annot_all.pkl"
+    Videos_Path: r"/kaggle/input/datasets/sherif31/group-activity-recognition-volleyball/videos"
+
 
     Config_file_path_Baseline1 = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline1.yml"
     Exp_Dir_Baseline1 = "/kaggle/working/Group-Activity-Recognition/modeling/baseline1/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
 
-    Config_file_path_Baseline3a = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline3a.yml"
+    Config_file_path_Baseline3a = r"/kaggle/working/group-activity-recognition/configs/Baseline3a.yml"
     Exp_Dir_Baseline3a = "/kaggle/working/Group-Activity-Recognition/modeling/baseline3a/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
 
-    Config_file_path_Baseline3b = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline3b.yml"
+    Config_file_path_Baseline3b = r"/kaggle/working/group-activity-recognition/configs/Baseline3b.yml"
     Exp_Dir_Baseline3b = "/kaggle/working/Group-Activity-Recognition/modeling/baseline3b/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
     
-    Config_file_path_Baseline4 = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline4.yml"
+    Config_file_path_Baseline4 = r"/kaggle/working/group-activity-recognition/configs/Baseline4.yml"
     Exp_Dir_Baseline4 = "/kaggle/working/Group-Activity-Recognition/modeling/baseline4/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
 
-    Config_file_path_Baseline5a = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline5a.yml"
+    Config_file_path_Baseline5a = r"/kaggle/working/group-activity-recognition/configs/Baseline5a.yml"
     Exp_Dir_Baseline5a = "/kaggle/working/Group-Activity-Recognition/modeling/baseline5a/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
     
-    Config_file_path_Baseline5b = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline5b.yml"
+    Config_file_path_Baseline5b = r"/kaggle/working/group-activity-recognition/configs/Baseline5b.yml"
     Exp_Dir_Baseline5b = "/kaggle/working/Group-Activity-Recognition/modeling/baseline5b/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
 
-    Config_file_path_Baseline6 = r"H:\Group-Activity-Recognition\group-activity-recognition\configs\Baseline6.yml"
+    Config_file_path_Baseline6 = r"/kaggle/working/group-activity-recognition/configs/Baseline6.yml"
     Exp_Dir_Baseline6 = "/kaggle/working/Group-Activity-Recognition/modeling/baseline6/outputs/Baseline_B1_Step_A_V1_2024_12_22_09_43"
     
 
